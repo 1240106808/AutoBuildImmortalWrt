367440eeedb61bcc3f06f82c945bc7dae4336dbf
1240106808 <1240106808@qq.com>
2026-03-08 11:18:46 +0800

    Update build24.sh

--- x86-64/build24.sh
+++ x86-64/build24.sh
@@ -28,6 +28,9 @@ else
   # 同步第三方软件仓库run/ipk
   echo "🔄 正在同步第三方软件仓库 Cloning run file repo..."
   git clone --depth=1 https://github.com/wukongdaily/store.git /tmp/store-run-repo
+  rm -f /tmp/store-run-repo/luci-app-store-0.1.32-1_all.run
+  rm -f /tmp/store-run-repo/luci-app-uninstall-v1.2.0.run
+  
 
   # 拷贝 run/x86 下所有 run 文件和ipk文件 到 extra-packages 目录
   mkdir -p /home/build/immortalwrt/extra-packages
